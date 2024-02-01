1bace97d87567e3e7567445f82a919139122eb0c
Lucas Batista <contact@lucasbatista.me>
2024-02-01 12:48:20 -0300

    Fix category products (#33)

--- bff/src/main/kotlin/me/lucasbatista/vienna/shopify/repository/ShopifyProductRepository.kt
+++ bff/src/main/kotlin/me/lucasbatista/vienna/shopify/repository/ShopifyProductRepository.kt
@@ -1,18 +1,31 @@
 package me.lucasbatista.vienna.shopify.repository
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import me.lucasbatista.vienna.sdk.entity.Product
 import me.lucasbatista.vienna.sdk.entity.ProductVariant
 import me.lucasbatista.vienna.sdk.repository.ProductRepository
 import me.lucasbatista.vienna.shopify.graphql.GetProductByIdQuery
+import me.lucasbatista.vienna.shopify.graphql.GetProductsByCollectionIdQuery
 import me.lucasbatista.vienna.shopify.graphql.GetProductsQuery
 import me.lucasbatista.vienna.shopify.graphql.ShopifyGraphQLClient
 import org.springframework.stereotype.Repository
-import java.net.URI
 import java.net.URL
+import me.lucasbatista.vienna.shopify.graphql.getproductsquery.Product as ShopifyProduct
 
 @Repository
-class ShopifyProductRepository(private val client: ShopifyGraphQLClient) : ProductRepository {
-    override fun findAllByCategoryId(id: String) = findAllByQuery("collection:$id")
+class ShopifyProductRepository(
+    private val client: ShopifyGraphQLClient,
+    private val objectMapper: ObjectMapper,
+) : ProductRepository {
+    override fun findAllByCategoryId(id: String): List<Product> {
+        val result = client.executeAsAdmin(
+            GetProductsByCollectionIdQuery(
+                GetProductsByCollectionIdQuery.Variables("gid://shopify/Collection/$id"),
+            ),
+        ).data!!.collection!!.products
+        return result.nodes.map(::mapProduct)
+    }
 
     override fun findAllByIds(ids: List<String>): List<Product> {
         if (ids.isEmpty()) return listOf()
@@ -23,7 +36,12 @@ class ShopifyProductRepository(private val client: ShopifyGraphQLClient) : Produ
                 query += " OR "
             }
         }
-        return findAllByQuery(query.trim())
+        val result = client.executeAsAdmin(
+            GetProductsQuery(
+                GetProductsQuery.Variables(query),
+            ),
+        ).data!!.products.nodes
+        return result.map(::mapProduct)
     }
 
     override fun findById(id: String): Product {
@@ -32,12 +50,17 @@ class ShopifyProductRepository(private val client: ShopifyGraphQLClient) : Produ
                 GetProductByIdQuery.Variables("gid://shopify/Product/$id"),
             ),
         ).data!!.product!!
+        return mapProduct(result)
+    }
+
+    private fun mapProduct(result: Any): Product {
+        val it = objectMapper.readValue<ShopifyProduct>(objectMapper.writeValueAsBytes(result))
         return Product(
-            id = result.id.split("/").last(),
-            title = result.title,
-            description = result.description,
-            images = result.images.nodes.map { URI(it.url).toURL() },
-            variants = result.variants.nodes.map {
+            id = it.id.split("/").last(),
+            images = it.images.nodes.map { URL(it.url) },
+            title = it.title,
+            description = it.description,
+            variants = it.variants.nodes.map {
                 ProductVariant(
                     id = it.id.split("/").last(),
                     originalPrice = it.compareAtPrice!!.amount.toDouble(),
@@ -45,28 +68,5 @@ class ShopifyProductRepository(private val client: ShopifyGraphQLClient) : Produ
                 )
             },
         )
-    }
-
-    private fun findAllByQuery(query: String): List<Product> {
-        val result = client.executeAsAdmin(
-            GetProductsQuery(
-                GetProductsQuery.Variables(query),
-            ),
-        ).data!!.products.nodes
-        return result.map { it ->
-            Product(
-                id = it.id.split("/").last(),
-                images = it.images.nodes.map { URL(it.url) },
-                title = it.title,
-                description = it.description,
-                variants = it.variants.nodes.map {
-                    ProductVariant(
-                        id = it.id.split("/").last(),
-                        originalPrice = it.compareAtPrice!!.amount.toDouble(),
-                        sellingPrice = it.price.amount.toDouble(),
-                    )
-                },
-            )
-        }
     }
 }

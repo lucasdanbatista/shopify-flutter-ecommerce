38621598bfde40b6585928f1129c0abbd48330be
Lucas Batista <contact@lucasbatista.me>
2024-02-23 17:09:51 -0300

    Fix generated file (#72)

--- mobile/lib/dtos/address_dto.g.dart
+++ mobile/lib/dtos/address_dto.g.dart
@@ -8,27 +8,23 @@ part of 'address_dto.dart';
 
 AddressDTO _$AddressDTOFromJson(Map<String, dynamic> json) => AddressDTO(
       id: json['id'] as String?,
-      recipientName: json['recipientName'] as String?,
-      street: json['street'] as String?,
-      buildingNumber: json['buildingNumber'] as String?,
-      neighborhood: json['neighborhood'] as String?,
-      line1: json['line1'] as String?,
-      line2: json['line2'] as String?,
+      recipientFirstName: json['recipientFirstName'] as String?,
+      recipientLastName: json['recipientLastName'] as String?,
+      addressLine1: json['addressLine1'] as String?,
+      addressLine2: json['addressLine2'] as String?,
+      zipcode: json['zipcode'] as String?,
       city: json['city'] as String?,
       state: json['state'] as String?,
-      zipcode: json['zipcode'] as String?,
     );
 
 Map<String, dynamic> _$AddressDTOToJson(AddressDTO instance) =>
     <String, dynamic>{
       'id': instance.id,
-      'recipientName': instance.recipientName,
-      'street': instance.street,
-      'buildingNumber': instance.buildingNumber,
-      'neighborhood': instance.neighborhood,
-      'line1': instance.line1,
-      'line2': instance.line2,
+      'recipientFirstName': instance.recipientFirstName,
+      'recipientLastName': instance.recipientLastName,
+      'addressLine1': instance.addressLine1,
+      'addressLine2': instance.addressLine2,
+      'zipcode': instance.zipcode,
       'city': instance.city,
       'state': instance.state,
-      'zipcode': instance.zipcode,
     };
